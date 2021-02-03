8aa687b0adc1bce8434d4f19854cd8a661b32eef
Helton Reis <heltonkreis@gmail.com>
2021-02-03 11:23:56 -0300

    [misc] Initial commit

---
+++ ConsoleHelper.Tests/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleHelper.Tests
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ColorTests();
+
+            Console.WriteLine();
+
+            MenuTests();
+        }
+
+        private static void ColorTests()
+        {
+            ColorConsole.WriteWrappedHeader("Color Console Examples");
+
+            Console.WriteLine("\nUsing a splash of color in your Console code more easily... (plain text)\n");
+
+            ColorConsole.WriteLine("Color me this - in Red", ConsoleColor.Red);
+
+            ColorConsole.WriteWrappedHeader("Off with their green Heads!", headerColor: ConsoleColor.Green);
+
+            ColorConsole.WriteWarning("\nWorking...\n");
+
+            Console.WriteLine("Writing some mixed colors: (plain text)");
+            ColorConsole.WriteEmbeddedColorLine("Launch the site with [darkcyan]https://localhost:5200[/darkcyan] and press [yellow]Ctrl-c[/yellow] to exit.\n");
+
+            ColorConsole.WriteSuccess("The operation completed successfully.");
+        }
+
+        private static void MenuTests()
+        {
+            string[] options = { "option_1", "option_2", "option_3", "option_4", "option_5", "option_6" };
+
+            int choice = CLIHelper.MenuChoice(true, 2, options);
+            if (choice != -1)
+                Console.WriteLine($"Your choice was: {options[choice]}");
+
+            Console.WriteLine();
+
+            List<int> choices = CLIHelper.MultipleChoice(2, options);
+            string result = string.Empty;
+            foreach (int i in choices)
+            {
+                result += $"{options[i]}, ";
+            }
+            Console.WriteLine($"Your choice was: {result.TrimEnd(',', ' ')}");
+        }
+    }
+}
---
+++ ConsoleHelper/CLIHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleHelper
+{
+    public static class CLIHelper
+    {
+        public static int MenuChoice(bool canCancel, int optionsPerLine = 1, params string[] options)
+        {
+            const int spacingPerLine = 14;
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            Console.CursorVisible = false;
+
+            (int startX, int startY) = Console.GetCursorPosition();
+
+            do
+            {
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+
+                    if (i == currentSelection)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                    Console.Write(options[i]);
+
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Length)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.CursorVisible = true;
+            Console.WriteLine();
+
+            return currentSelection;
+        }
+
+        public static List<int> MultipleChoice(int optionsPerLine = 1, params string[] options)
+        {
+            const int spacingPerLine = 14;
+
+            List<int> result = new();
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            Console.CursorVisible = false;
+
+            (int startX, int startY) = Console.GetCursorPosition();
+
+            do
+            {
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+
+                    if (i == currentSelection)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                    if (result.Contains(i))
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.Write(options[i]);
+
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Length)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+
+                    case ConsoleKey.Enter:
+                        {
+                            if (!result.Contains(currentSelection))
+                                result.Add(currentSelection);
+                            else
+                                _ = result.Remove(currentSelection);
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Escape);
+
+            Console.CursorVisible = true;
+
+            Console.WriteLine();
+
+            return result;
+        }
+    }
+}
